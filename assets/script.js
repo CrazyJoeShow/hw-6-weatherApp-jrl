630b7c561e1b42283cf10bb0aa9fc4bc5a5cdd61
Joe Lowery <joseph.loweryit@gmail.com>
2020-09-15 20:31:20 -0400

    query call and log of response and that query

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,28 @@
 console.log('Hello! ya old fogey!');
+
+
+//---------search Function---------//
+
+function searchCity(cityname) {
+
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&units=imperial&appid=ecc0be5fd92206da3aa90cc41c13ca56";
+    var queryURLforcast = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityname + "&units=imperial&appid=ecc0be5fd92206da3aa90cc41c13ca56";
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response){
+    console.log(response);
+    console.log(queryURL)
+    $("#current").empty();
+    var mainDate = moment().format('L');
+
+
+    //--------HTML to put the information in-----//
+    var cityNameEl = $("<h2>").text(response.name);
+        var displayMainDate = cityNameEl.append(" " + mainDate);
+        var tempEL = $("<p>").text("Tempraturer: " + response.main.temp);
+        var humEl = $("<p>").text("Humidity: " + response.main.humidity);
+        var windEl = $("<p>").text("Wind Speed: " + response.wind.speed);
+        var currentweather = response.weather[0].main;
+
+})
